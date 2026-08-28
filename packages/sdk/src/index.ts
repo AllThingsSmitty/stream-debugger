539002a52a7bd7b5e1d7bd215e0efbb9e757438c
Matt Smith <runner_28@hotmail.com>
2026-08-28 10:28:01 -0400

    Add types and config

--- packages/sdk/src/index.ts
+++ packages/sdk/src/index.ts
@@ -1,5 +1,15 @@
 export const version = '0.1.0';
 
+// Core re-exports (types and utilities)
+export { serializeStream, deserializeStream } from '@stream-debugger/core';
+export type {
+  StreamDocument,
+  StreamMetadata,
+  StreamEvent,
+  StreamSummary,
+  WaterfallData,
+} from '@stream-debugger/core';
+
 // Adapter types
 export type { StreamAdapter, StreamCapture, CaptureConfig } from './types/adapter';
 
