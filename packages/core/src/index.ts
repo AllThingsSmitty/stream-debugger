fceaeb18376aa6f52e4d7dd6e020fa9b626ca97e
Matt Smith <runner_28@hotmail.com>
2026-08-28 09:59:50 -0400

    Add documentation

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,1 +1,18 @@
 export const version = '0.1.0';
+
+// .stream format types and utilities
+export { STREAM_FORMAT_VERSION } from './types/stream';
+export type {
+  StreamDocument,
+  StreamMetadata,
+  StreamEvent,
+  StreamSummary,
+  WaterfallData,
+  TimelineSegment,
+  Keyframe,
+  RequestContext,
+  ResponseContext,
+  ProviderEvent,
+} from './types/stream';
+
+export { StreamBuilder, serializeStream, deserializeStream } from './stream-builder';
